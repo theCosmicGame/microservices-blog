c23a20c8a4dd69c9a3755d7ca51b50184aefff0c
Bridget Melvin <bridgetmelvin42@gmail.com>
2022-05-16 06:43:27 -0400

    Commit: began setting up Docker

--- posts/index.js
+++ posts/index.js
@@ -26,6 +26,7 @@ app.post("/posts", async (req, res) => {
     title,
   };
   
+  // PostCreated resource
   await axios.post("http://localhost:4005/events", {
     type: "PostCreated",
     data: {
